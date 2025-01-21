7d8767ac031660d7bdfbcb2307323392ddb670eb
MUDDASIR-786 <alimuddasir436@gmail.com>
2025-01-21 16:36:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+// // datatyes
+// //1)string
+// //2)number
+
+
+
+
+
+
+
+
+// document.write(2 + " X " + 1 + " = " + 2*1 + "<BR>")
+// document.write(2 + " X " + 2 + " = " + 2*2 + "<BR>")
+// document.write(2 + " X " + 3 + " = " + 2*3 + "<BR>")
+// document.write(2 + " X " + 4 + " = " + 2*4 + "<BR>")
+// document.write(2 + " X " + 5 + " = " + 2*5 + "<BR>")
+// document.write(2 + " X " + 6 + " = " + 2*6 + "<BR>")
+// document.write(2 + " X " + 7 + " = " + 2*7 + "<BR>")
+// document.write(2 + " X " + 8 + " = " + 2*8 + "<BR>")
+// document.write(2 + " X " + 9 + " = " + 2*9 + "<BR>")
+// document.write(2 + " X " + 10 + " = " + 2*10 + "<BR>" + "<br>")
+
+
+
+
+
+
+
+// document.write(125 + " X " + 1 + " = " + 125*1 + "<BR>")
+// document.write(125 + " X " + 2 + " = " + 125*2 + "<BR>")
+// document.write(125 + " X " + 3 + " = " + 125*3 + "<BR>")
+// document.write(125 + " X " + 4 + " = " + 125*4 + "<BR>")
+// document.write(125 + " X " + 5 + " = " + 125*5 + "<BR>")
+// document.write(125 + " X " + 6 + " = " + 125*6 + "<BR>")
+// document.write(125 + " X " + 7 + " = " + 125*7 + "<BR>")
+// document.write(125 + " X " + 8 + " = " + 125*8 + "<BR>")
+// document.write(125 + " X " + 9 + " = " + 125*9 + "<BR>")
+// document.write(125 + " X " + 10 + " = " + 125*10 + "<BR>")
+
+
+// document .write(4 + " X " + " 1 "  + )
+
+
+
+
+// var  userInput = prompt("enter your name")
+// var userEmail = prompt('enter email')
+// document.write(userInput)
+
+
+
+// document.write(userEmail)
+
+// var userInput = prompt ("Enter your name")
+
+// alert (userInput)
+
+// document .write(userInput)
+
+
+// var helloWorld = prompt ("Enter your name")
+
+var keyWord = prompt ("Enter your name")
+alert ( "Welcome " + keyWord )
+document .write(keyWord)
